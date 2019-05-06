d5584ad53a4de58b138d6574c181f58011ded004
Antonia Siu <mail@antoniasiu.co.uk>
2019-05-06 17:50:00 +0100

    add preferred .eslintrc.js settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,47 @@ module.exports = {
     node: true,
   },
   extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
     'plugin:vue/essential',
+    'plugin:jest/recommended',
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': [process.env.NODE_ENV === 'production' ? 'error' : 'warn', {
+      argsIgnorePattern: '^_',
+    }],
+    'no-underscore-dangle': 'off',
+    'no-warning-comments': 'warn',
+    'prefer-destructuring': ['warn', {
+      AssignmentExpression: {
+        array: false,
+      },
+    }],
+    'import/extensions': ['error', {
+      js: 'never',
+      vue: 'ignorePackages',
+    }],
+    'import/order': ['error', {
+      groups: ['index', 'sibling', 'parent', 'internal', 'external', 'builtin'],
+      'newlines-between': 'never',
+    }],
+    'jest/no-test-prefixes': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
+  },
+  settings: {
+    'import/extensions': [
+      '.js',
+      '.vue',
+    ],
+    'import/resolver': {
+      webpack: {
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
+    },
   },
 };
